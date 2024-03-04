c61ca403cf88a16e48d40cd96384ea8cf26c5909
D3struf <johnpaul.monter@tup.edu.ph>
2024-03-04 23:09:05 +0800

    first commit

--- app.py
+++ app.py
@@ -7,12 +7,13 @@
 #             [2, 1, 4, 3])
 
 def printMatrix(matrix):
+    print("MATRIX: ")
     for xlist in matrix:
         for ylist in xlist:
             print(ylist, end=" ")
         print()
 
-def getValue (matrix):
+def getValue (matrix, x, y):
     for i in range(len(matrix)):
         if i == int(x):
             for j in range(len(matrix)):
@@ -31,7 +32,7 @@ def getCoords():
             if coords[1] != ',' or (int(x) > 4 or int(y) > 4):
                 print("Invalid Coordinates!! Try Again...")
                 return getCoords()
-        else: return coords 
+        else: return x, y
     except ValueError:
         print("Invalid Coordinates!! Try Again...")
         return getCoords()
@@ -54,17 +55,9 @@ if __name__ == '__main__':
     while askAgain == 'y':
         printMatrix(matrix)
         
-        askUser = getCoords();
-
-        x, y = askUser.split(",")
-        print("x: " + x)
-        print("y: " + y)
+        x, y = getCoords();
         
-        print("Value: ", getValue(matrix))
-        print("MATRIX: ")
-        for xlist in matrix:
-            for ylist in xlist:
-                print(ylist, end=" ")
-            print()
-            
+        print("Value: ", getValue(matrix, x, y))
+        printMatrix(matrix)
+        
         askAgain = input("Would you like to enter another Value? (y or n): ")
