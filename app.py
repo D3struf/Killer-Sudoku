29adb7ebe8454d0631f5ee3094f2692bb299379e
D3struf <johnpaul.monter@tup.edu.ph>
2024-03-06 00:13:46 +0800

    added cages

--- app.py
+++ app.py
@@ -5,9 +5,12 @@
 #             [4, 3, 2, 1],
 #             [3, 4, 1, 2],
 #             [2, 1, 4, 3])
-# Each row or col has a sum of 10 
-# Each group has a sum
-# If a group of 1 is the same as the number
+# Each row should have numbers 1 to 4 and does not have a duplicate.
+# Each column should have numbers 1 to 4 and does not have a duplicate.
+# Each row, col, and 2x2 subgrid should have a sum of 10.
+# Each group has a sum entered by the user.
+# If a group has only one cell then the sum and the number should be the same.
+import sys
 
 def printMatrix(matrix):
     print("MATRIX: ")
@@ -22,53 +25,131 @@ def printMatrix(matrix):
             print(j, end=" ")
         count = 0
         print()
+        
+def checkDuplicate(matrix, x, y, value, check):
+    if check == 'row':
+        return value in matrix[int(x)]
+    elif check == 'col':
+        return value in [matrix[i][int(y)] for i in range(4)]
+    elif check == 'subgrid':
+        subgrid_values = [matrix[i][j] for i in range(2 * (int(x) // 2), 2 * (int(x) // 2) + 2) for j in range(2 * (int(y) // 2), 2 * (int(y) // 2) + 2)]
+        return value in subgrid_values
 
-def getValue (matrix, x, y):
-    for i in range(len(matrix)):
-        if i == int(x):
-            for j in range(len(matrix)):
-                if j == int(y):
-                    askUserValue = userValue()
-                    matrix[i][j] = askUserValue
-                    return askUserValue
-                
+def getValue(matrix, x, y):
+    try:
+        askUserValue = userValue()
+        if not checkDuplicate(matrix, x, y, askUserValue, 'row') and \
+            not checkDuplicate(matrix, x, y, askUserValue, 'col') and \
+            not checkDuplicate(matrix, x, y, askUserValue, 'subgrid'):
+            matrix[int(x)][int(y)] = askUserValue
+            return askUserValue
+        else:
+            print("Duplicate number found in row, column, or subgrid. Try again.")
+            return getValue(matrix, x, y)
+    except ValueError:
+        print("Please Enter a Number")
+        return getValue(matrix, x, y)
+
 def userValue():
     try:
-        value = int(input("Please Enter Value: "))
+        value = int(promptAgain('userVal'))
         if (value > 4):
             print("Please Enter Number less than 4")
+            return userValue()
+        elif (value == 0):
+            print("Please Enter a Number from 1 to 4")
             return userValue()
         return value
     except ValueError:
         print("Please Enter a Number")
         return userValue()
-                
+
 def getCoords():
     coords = ''
     try:
         while len(coords) != 3:
-            coords = input("Please Enter coordinates (Ex. x,y): ")
+            coords = promptAgain('coords')
             x, y = coords.split(',')
-
-            if coords[1] != ',' or (int(x) > 4 or int(y) > 4):
+            
+            if coords[1] != ',' or (int(x) >= 4 or int(y) >= 4):
                 print("Invalid Coordinates!! Try Again...")
+                return getCoords()
+            elif matrix[int(x)][int(y)] != 0:
+                print("Cell already has a number. Try again.")
                 return getCoords()
         else: return x, y
     except ValueError:
         print("Invalid Coordinates!! Try Again...")
         return getCoords()
+    
+def initializeMatrix():
+    return [[0 for _ in range(4)] for _ in range(4)]
+
+def promptAgain(prompt):
+    try:
+        if prompt == 'again':
+            # Continue when Enter key is pressed
+            ask = input("Would you like to enter another Value? (y or n): ") or 'y'
+            if ask == 'y' or ask == 'n':
+                return ask
+            promptAgain('again')
+        elif prompt == 'coords':
+            ask = input("Please Enter coordinates (Ex. x,y): ")
+            return ask
+        elif prompt == 'userVal':
+            ask = input("Please Enter Value: ")
+            return ask
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+        sys.exit()
+        
+def getCageInput(cages):
+    print("FORMAT: (row, col)")
+    print("NOTE: Just press Enter to close the cage input.")
+    cage_sets = []
+    while True:
+        cages_input = cagePrompt()
+        if cages_input == ' ':
+            break
+        cage_sets.append(cages_input)
+
+    for cage_set in cage_sets:
+        cells = tuple(map(int, cage_set.split(",")))
+        cages.append(cells)
+    print("Cages:", cages)
+    
+def cagePrompt():
+    coords = ''
+    try:
+        while len(coords) != 3:
+            coords = input('Enter Cage Cell: ')
+            
+            x, y = coords.split(',')
+            if coords[1] != ',' or (int(x) >= 4 or int(y) >= 4):
+                print("Invalid Coordinates!! Try Again...")
+                return cagePrompt()
+        else: 
+            return coords
+    except ValueError:
+        print("Invalid Coordinates!! Try Again...")
+        return cagePrompt()
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+        sys.exit()
 
 if __name__ == '__main__':
-    # Initialize the 4x4 matrix
-    matrix = [[0 for _ in range(4)] for _ in range(4)]
+    matrix = initializeMatrix()
     askAgain = 'y'
+    cages = []
     
-    while askAgain == 'y':
-        printMatrix(matrix)
+    getCageInput(cages)
+    
+    # while askAgain == 'y':
+    #     printMatrix(matrix)
         
-        x, y = getCoords();
+    #     x, y = getCoords();
         
-        print("Value: ", getValue(matrix, x, y))
-        printMatrix(matrix)
+    #     print("Value: ", getValue(matrix, x, y))
+    #     printMatrix(matrix)
         
-        askAgain = input("Would you like to enter another Value? (y or n): ")
+    #     askAgain = promptAgain('again')
